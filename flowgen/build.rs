f013079ba8b39bb632f12a0216e822c1c48f862e
Jakub Dulny <jdulny@connve.com>
2024-07-29 13:13:11 +0200

    Fix for wrong path in build.rs

--- flowgen/build.rs
+++ flowgen/build.rs
@@ -1,21 +1,21 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Google gRPC APIs.
     // https://github.com/googleapis/googleapis
-    // tonic_build::configure()
-    //     .build_client(true)
-    //     .build_server(false)
-    //     .out_dir("/src/google")
-    //     .compile(
-    //         &["proto/googleapis/google/storage/v2/storage.proto"],
-    //         &["proto/googleapis"],
-    //     )?;
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(false)
+        .out_dir("src/google")
+        .compile(
+            &["proto/googleapis/google/storage/v2/storage.proto"],
+            &["proto/googleapis"],
+        )?;
 
     // Salesforce PubSub.
     // https://github.com/forcedotcom/pub-sub-api
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
-        .out_dir("/src/salesforce")
+        .out_dir("src/salesforce")
         .compile(
             &["proto/salesforce/pubsub/pubsub.proto"],
             &["proto/salesforce/pubsub"],
