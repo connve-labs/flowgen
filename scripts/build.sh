4521088c4936821fce87abdcd1f85cd556352eb2
Jakub Dulny <jdulny@connve.com>
2024-08-28 11:59:19 +0200

    Try with cloning repo

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e -x
+apt update -qq
+apt-get -qq install pkg-config libssl-dev protobuf-compiler
+apt-get install git
+cd components/google/proto && git clone https://github.com/googleapis/googleapis
+cd components/salesforce/proto && git clone https://github.com/forcedotcom/pub-sub-api
+cargo build --verbose
+cargo test --verbose
